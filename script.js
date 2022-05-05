7dfdf5f8d367bf5b15e4da7934dd8edf38355d94
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-05-05 17:11:32 -0400

    playing audio from keydwon event

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window.addEventListener('keydown', function (e) {
-  console.log(e);
+  console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return; //stop function running
+  console.log(audio);
+  audio.play();
 });
