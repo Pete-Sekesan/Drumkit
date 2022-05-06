005828d43d54cc5a831a72906bc719ba4f64a19e
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-05-06 10:31:25 -0400

    transition add/remove function added

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
   console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; //stop function running
   console.log(audio);
+  audio.currentTime = 0; //rewind to start
   audio.play();
-});
+  key.classList.add('playing');
+
+  console.log(key);
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
